53420876c1fd9f28aa3f1066f4ae2e5efb388baf
Thuvh94 <aloha.hat.2508@gmail.com>
2020-09-07 16:39:28 +0700

    final

--- task-1/task1.js
+++ task-1/task1.js
@@ -1,8 +1,7 @@
 function calculateEquation(a,b) {
-    let x = 0;
     if(a===0)
         alert("Hãy nhập a khác 0");
     else
-        x = (-b)/a;
+        let x = (-b)/a;
         return x;
 }
--- task-2/task2.js
+++ task-2/task2.js
@@ -1,10 +1,11 @@
 function findElement(num,arr) {
-    let position = []
-    if (num ==="")
+    let position = [];
+    if (num ===""){
         alert('Hãy nhập lại');
         return;
+    }
     for (let i = 0; i < arr.length; i++) {
-        if (num == arr[i])
+        if (num === arr[i])
             position.push(i);
     }
     return position;
