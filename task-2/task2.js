78acb075a93dfbdc6168771a1c40c521c357bd18
Thuvh94 <aloha.hat.2508@gmail.com>
2020-09-07 16:27:19 +0700

    third commit

--- task-2/task2.js
+++ task-2/task2.js
@@ -1,5 +1,8 @@
 function findElement(num,arr) {
     let position = []
+    if (num ==="")
+        alert('Hãy nhập lại');
+        return;
     for (let i = 0; i < arr.length; i++) {
         if (num == arr[i])
             position.push(i);
